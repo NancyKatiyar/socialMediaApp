301ec0caf7607d020b32a7ce90080358a8c91ae3
NancyKatiyar <nancykatiyar74@gmail.com>
2025-11-04 17:58:55 +0530

    cors added

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv")
-dotenv.config();
-app.use(express.json());
+const dotenv = require("dotenv");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
+
+
+dotenv.config();
+
+app.use(cors({
+  origin: "http://127.0.0.1:5500",
+  credentials: true,
+}));
+app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static('uploads'));
 
+app.use(express.static(path.join(__dirname, "views/instagram-clone")));
+app.use(express.static("uploads"));
 
+app.get("/", (req, res) => {
+  res.redirect("/pages/login.html");
+});
+
+// Routes
 const userRoutes = require("./routes/userRoutes");
-const postRouter = require("./routes/postRoutes")
-const commentRouter = require("./routes/commentsRoutes")
-app.use("/",userRoutes)
-app.use("/",postRouter)
-app.use("/",commentRouter)
+const postRouter = require("./routes/postRoutes");
+const commentRouter = require("./routes/commentsRoutes");
 
+app.use("/", userRoutes);
+app.use("/", postRouter);
+app.use("/", commentRouter);
 
-
-
-
-
-
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
